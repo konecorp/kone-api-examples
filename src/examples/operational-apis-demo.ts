64011009ca2e36490ed31608bb043ee0329c214d
htran <hieu.tran@kone.com>
2021-04-26 17:17:24 +0300

    fetch acccessible equipments by using default access token

--- src/examples/operational-apis-demo.ts
+++ src/examples/operational-apis-demo.ts
@@ -1,13 +1,12 @@
 import _ from 'lodash'
-import { fetchAccessToken } from '../common/koneapi'
+import { fetchAccessToken, fetchResources } from '../common/koneapi'
 import { fetchEquipmentBasicInformation, fetchEquipmentStatus, fetchServiceOrdersList, fetchSingleServiceOrder } from '../common/operationalapi'
 
 /**
- * Update these three variables with your own credentials and equipment identifier or set them up as environment variables.
+ * Update these two variables with your own credentials and equipment identifier or set them up as environment variables.
  */
 const CLIENT_ID: string = process.env.CLIENT_ID || 'YOUR_CLIENT_ID' // eg. 'dcf48ab0-a902-4b52-8c53-1a9aede716e5'
 const CLIENT_SECRET: string = process.env.CLIENT_SECRET || 'YOUR_CLIENT_SECRET' // eg. '31d1329f8344fc12b1a960c8b8e0fc6a22ea7c35774c807a4fcabec4ffc8ae5b'
-const EQUIPMENT_ID_WITH_PREFIX: string = process.env.EQUIPMENT_ID_WITH_PREFIX || 'YOUR_EQUIPMENT_ID_WITH_KEN_PREFIX' // eg. sanbox equipment ken:9999999999
 
 /**
  * Check if the needed variables have been defined
@@ -17,27 +16,23 @@ const checkRequiredVariables = () => {
     _.isEmpty(CLIENT_ID) ||
     _.isEmpty(CLIENT_SECRET) ||
     CLIENT_ID === 'YOUR_CLIENT_ID' ||
-    CLIENT_SECRET === 'YOUR_CLIENT_SECRET' ||
-    EQUIPMENT_ID_WITH_PREFIX === 'YOUR_EQUIPMENT_ID_WITH_KEN_PREFIX'
+    CLIENT_SECRET === 'YOUR_CLIENT_SECRET'
   )
-  throw Error('CLIENT_ID, CLIENT_SECRET, and EQUIPMENT_ID_WITH_PREFIX need to be defined')
+  throw Error('CLIENT_ID and CLIENT_SECRET need to be defined')
 }
 
-const start = async () => {
-  try {
-    checkRequiredVariables()
-  } catch (error) {
-    console.error(error.message)
-    return
-  }
-
-  let accessToken = await fetchAccessToken(CLIENT_ID, CLIENT_SECRET, ['equipmentstatus/*', 'serviceinfo/*'])
-  console.log('AccessToken successfully fetched')
+/**
+ * Demo equipment status api by fetching basic information of the equipment and its maintenance status
+ */
+const demoEquipmentStatusApi = async (accessToken: string, targetEquipmentId: string) => {
+  const scopes = [`equipmentstatus/${targetEquipmentId}`]
+  accessToken = await fetchAccessToken(CLIENT_ID, CLIENT_SECRET, scopes)
+  console.log(`AccessToken with scope ${scopes} successfully fetched`)
 
   // Fetch basic information of equipment
   try {
-    console.log(`Fetch basic information of the equipment ${EQUIPMENT_ID_WITH_PREFIX}`)
-    const equipmentInfo = await fetchEquipmentBasicInformation(accessToken, EQUIPMENT_ID_WITH_PREFIX)
+    console.log(`Fetch basic information of the equipment ${targetEquipmentId}`)
+    const equipmentInfo = await fetchEquipmentBasicInformation(accessToken, targetEquipmentId)
     console.log(equipmentInfo)
   } catch (error) {
     console.error('Failed to fetch information of the equipment')
@@ -45,18 +40,27 @@ const start = async () => {
 
   // Fetch maintenance status of equipment
   try {
-    console.log(`Fetch maintenance status of the equipment ${EQUIPMENT_ID_WITH_PREFIX}`)
-    const equipmentStatus = await fetchEquipmentStatus(accessToken, EQUIPMENT_ID_WITH_PREFIX)
+    console.log(`Fetch maintenance status of the equipment ${targetEquipmentId}`)
+    const equipmentStatus = await fetchEquipmentStatus(accessToken, targetEquipmentId)
     console.log(equipmentStatus)
   } catch (error) {
     console.error('Failed to fetch maintenance status')
   }
-  
+}
+
+/**
+ * Demo service info api by fetching list of service orders and detail of a order for the equipment
+ */
+const demoServiceInfoApi = async (accessToken: string, targetEquipmentId: string) => {
+  const scopes = [`serviceinfo/${targetEquipmentId}`]
+  accessToken = await fetchAccessToken(CLIENT_ID, CLIENT_SECRET, scopes)
+  console.log(`AccessToken with scope ${scopes} successfully fetched`)
+ 
   // Fetch list of all service orders
   let serviceOrdersList
   try {
-    console.log(`Fetch list of service orders for the equipment ${EQUIPMENT_ID_WITH_PREFIX}`)
-    serviceOrdersList = await fetchServiceOrdersList(accessToken, EQUIPMENT_ID_WITH_PREFIX)
+    console.log(`Fetch list of service orders for the equipment ${targetEquipmentId}`)
+    serviceOrdersList = await fetchServiceOrdersList(accessToken, targetEquipmentId)
     console.log(serviceOrdersList)
   } catch (error) {
     console.error('Failed to fetch list of service orders')
@@ -68,12 +72,35 @@ const start = async () => {
       return
     }
     const { serviceOrderId } = serviceOrdersList[0]
-    console.log(`Fetch details of service order id ${serviceOrderId} for equipment ${EQUIPMENT_ID_WITH_PREFIX}`)
-    const singleServiceOrder = await fetchSingleServiceOrder(accessToken, EQUIPMENT_ID_WITH_PREFIX, serviceOrderId)
+    console.log(`Fetch details of service order id ${serviceOrderId} for equipment ${targetEquipmentId}`)
+    const singleServiceOrder = await fetchSingleServiceOrder(accessToken, targetEquipmentId, serviceOrderId)
     console.log(singleServiceOrder)
   } catch (error) {
     console.error('Failed to fetch details of the service order')
   }
+}
+
+const start = async () => {
+  try {
+    checkRequiredVariables()
+  } catch (error) {
+    console.error(error.message)
+    return
+  }
+
+  // Fetch the first token which will by default contain application/inventory scope for our use in the next request
+  let accessToken = await fetchAccessToken(CLIENT_ID, CLIENT_SECRET)
+  console.log('AccessToken successfully fetched')
+  
+  // Fetch equipment ids to which user has access to 
+  const equipments = await fetchResources(accessToken, 'ken')
+  console.log('List of accessible equipments:', equipments)
+  
+  const targetEquipmentId = equipments[0]
+
+  await demoEquipmentStatusApi(accessToken, targetEquipmentId)
+  await demoServiceInfoApi(accessToken, targetEquipmentId)
+  
 }
 
 start()
